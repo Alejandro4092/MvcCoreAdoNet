1c067c4ca4164ab97445d4c3add158410a614f75
Alejandro4092 <acobomarcos@gmail.com>
2026-01-26 14:19:20 +0100

    Add project files.

---
+++ MvcCoreAdoNet/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreAdoNet.Models;
+
+namespace MvcCoreAdoNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Vista1()
+        {
+            return View();
+        }
+        public IActionResult VistaReturn()
+        {
+            return View();
+        }
+        public IActionResult VistaPersona()
+        {
+         
+            return View();
+        }
+        public IActionResult VistaDosImg()
+        {
+            return View();
+        }
+    }
+}
